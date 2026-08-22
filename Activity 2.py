2925c2e1bfd63a7ad71274b57e431ac1941384c8
viraajestein <viraaj.gupta@pathwaysschool.in>
2026-08-22 15:37:35 +0530

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,26 @@
+print("Hello everyone!")
+
+a = 10
+b = 12
+c = 12
+
+print("\n",not(a == b))
+
+print("\n",not(b == c))
+
+a = "Coding"
+b = "Codingal"
+
+if not (a == b):
+    print("\n",a, "and", b, "are not equal.")
+
+a = 4
+a = 5
+
+if not((a == 1) == (b == 5)):
+    print("Hello")
+
+a = int(input("\nPlease enter a number: "))
+
+if not (a % 2 == 0):
+    print("\nThe number you entered is odd.")
